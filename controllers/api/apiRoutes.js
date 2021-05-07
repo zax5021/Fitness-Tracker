f55557cdb8c7a645bc2c991084459a8c34f908b6
zax5021 <zacharyacampbell1@gmail.com>
2021-05-07 16:16:29 -0700

    added most of the routes

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -1,4 +1,48 @@
 const router = require("express").Router();
-const db = require("../models");
+const db = require("../../models");
 
-// router.get();
+// get all
+router.get("/", async (req, res) => {
+    try {
+        const lastOne = await db.Workout.findAll().sort({_id: 1});
+        res.status(200).json(lastOne);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// add
+router.post("/", async (req, res) => {
+    try {
+        const newWorkout = await db.Workout.create(req.body);
+        res.status(200).json(newWorkout);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// update
+router.put("/:id", async (req, res) => {
+    try {
+        const newRoutine = await db.Workout.findByIdAndUpdate(req.params.id,
+            { $push: { exercises: req.body }},
+            { new: true}
+            );
+        res.status(200).json(newRoutine);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// get last 7 workouts
+router.get("/range", async (req, res)=> {
+try {
+    const range = await db.Workout.findAll().sort({_id: -1}).limit(7);
+    res.status(200).json(reverse);
+} catch (err) {
+    res.status(500).json(error);
+}
+
+});
+
+module.exports = router;
